75927bfdc34258bced58861d4a62e62d03683881
Leonov Artem <leonovru@yandex.ru>
2019-02-05 17:08:24 +0300

    Add new files

---
+++ Huffman Codes/BinaryTree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman_Codes
+{
+    class BinaryTree
+    {
+        public Node Root { get; set; }
+    }
+}
---
+++ Huffman Codes/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman_Codes
+{
+    class Node
+    {
+        public int Frequency { get; set; }
+        public string Symbol { get; set; }
+
+        Node LeftChild;
+        Node RightChild;
+    }
+}
